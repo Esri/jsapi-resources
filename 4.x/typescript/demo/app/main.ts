7faed4b5fdaeff1dfdcfd2d889bbe48177b5bca6
Gavin Rehkemper <gavinr@users.noreply.github.com>
2017-07-06 08:46:19 -0500

    Modified to remove typescript errors

--- 4.x/typescript/demo/app/main.ts
+++ 4.x/typescript/demo/app/main.ts
@@ -1,13 +1,18 @@
 import EsriMap = require("esri/Map");
 import MapView = require("esri/views/MapView");
+import Basemap = require("esri/Basemap");
+import Point = require("esri/geometry/Point");
 
 const map = new EsriMap({
-  basemap: "streets"
+  basemap: "streets" as any as Basemap
 });
 
 const view = new MapView({
   map: map,
   container: "viewDiv",
-  center: [-118.244, 34.052],
+  center: new Point({
+    x: -118.244,
+    y: 34.052
+  }),
   zoom: 12
 });
