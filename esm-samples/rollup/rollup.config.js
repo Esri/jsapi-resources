fd7689c9d0f568a5343aeab913eefed94ee4fa3b
Andy G <agup2001@msn.com>
2021-02-26 14:43:15 -0700

    Rollup sample - adjust copy path, remove clean and reorder imports

--- esm-samples/rollup/rollup.config.js
+++ esm-samples/rollup/rollup.config.js
@@ -1,9 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
+import copy from 'rollup-plugin-copy';
 import del from "rollup-plugin-delete";
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-import copy from 'rollup-plugin-copy';
-import clean from 'rollup-plugin-clean';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -17,17 +16,16 @@ export default {
     format: "es"
   },
   plugins: [
-    clean(),
     del({ targets: ["public/chunks", "public/assets"], runOnce: true, verbose: true }),
     copy({
       // Copy the ArcGIS API for JavaScript assets
       targets: [
-        { src: './node_modules/@arcgis/core/assets/*', dest: './public/assets'},
+        { src: './node_modules/@arcgis/core/assets', dest: './public'},
       ],
       copyOnce: true
     }),    
     resolve(),
-    commonjs(),
+    commonjs(), 
     production && terser()
   ],
   preserveEntrySignatures: false
