116485ec46e6efa139edb0d277fbb679f381e0e4
Andy G <agup@esri.com>
2021-03-30 16:23:07 -0600

    Rollup sample - add production config

---
+++ esm-samples/rollup/rollup.config.prod.js
@@ -0,0 +1,20 @@
+import commonjs from "@rollup/plugin-commonjs";
+import del from "rollup-plugin-delete";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/main.js",
+  output: {
+    chunkFileNames: "chunks/[name]-[hash].js",
+    dir: "public",
+    format: "es"
+  },
+  plugins: [
+    del({ targets: ["public/chunks"], runOnce: true, verbose: true }),
+    resolve(),
+    commonjs(), 
+    terser()
+  ],
+  preserveEntrySignatures: false
+};
