bf24e1fba4612cb2dd1ceed4dc1a4914dd43d163
Andy G <agup@esri.com>
2021-03-30 16:16:41 -0600

    Node sample - update asset copy code.

--- esm-samples/jsapi-node/rollup.config.js
+++ esm-samples/jsapi-node/rollup.config.js
@@ -1,14 +1,25 @@
 import commonjs from "@rollup/plugin-commonjs";
+import copy from "rollup-plugin-copy";
+import del from "rollup-plugin-delete";
 import resolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: ["src/request.js", "src/projection.js", "src/webmap.js"],
   output: {
     chunkFileNames: "chunks/[name].js",
-    dir: "dist",
+    dir: "public",
     format: "cjs"
   },
   external: ["whatwg-fetch"],
-  plugins: [resolve(), commonjs()],
+  plugins: [
+    del({ targets: ["public/chunks", "public/assets"], runOnce: true, verbose: true }),
+    copy({
+      // Copy the ArcGIS API for JavaScript assets
+      targets: [{ src: "./node_modules/@arcgis/core/assets", dest: "./public" }],
+      copyOnce: true
+    }),
+    resolve(),
+    commonjs()
+  ],
   preserveEntrySignatures: false
 };
--- esm-samples/jsapi-node/src/projection.js
+++ esm-samples/jsapi-node/src/projection.js
@@ -3,7 +3,7 @@ import * as pe from "@arcgis/core/geometry/projection.js";
 import Point from "@arcgis/core/geometry/Point.js";
 
 // Required: Set this property to insure assets resolve correctly.
-esriConfig.assetsPath = "node_modules/@arcgis/core/assets"; // relative to when running in root
+esriConfig.assetsPath = "./public/assets"; // relative to when running in root
 
 pe.load()
   .then(() => {
