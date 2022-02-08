ffd1be6ecd4fa1be0430b6839479d10aeec342d5
Ben Elan <benelan@users.noreply.github.com>
2022-02-08 12:50:02 -0800

    ci: create script to analyze esm sample build sizes (#371)
    * ci: create script to analyze esm sample build sizes
    
    * ci: cleanup build size script
    
    * cleanup
    
    * run on push
    
    * remove comments for alt option
    
    * populate sample framework version from package
    
    * ci: use correct main bundle file size
    
    * cleanup
    
    * run action on PR events
    
    * dont fail when there is nothing to commit
    
    * ci: analyze esm sample builds
    
    * perf: decrease build script execution time
    
    * use PAT and change output directory
    
    * small change to trigger build
    
    * ci: analyze esm sample builds
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .github/scripts/analyze-builds.js
@@ -0,0 +1,118 @@
+const { resolve } = require("path");
+const { promisify } = require("util");
+const {
+  createWriteStream,
+  promises: { readdir, readFile }
+} = require("fs");
+const exec = promisify(require("child_process").exec);
+
+const SAMPLES_PATH = resolve(__dirname, "../../esm-samples");
+
+const SAMPLES_INFO = {
+  "jsapi-angular-cli": {
+    name: "Angular",
+    package: "@angular/core",
+    buildDirectory: "dist",
+    bundleDirectory: "./"
+  },
+  "jsapi-create-react-app": {
+    name: "CRA",
+    package: "react-scripts",
+    buildDirectory: "build",
+    bundleDirectory: "static/js"
+  },
+  "jsapi-vue-cli": {
+    name: "Vue",
+    package: "vue",
+    buildDirectory: "dist",
+    bundleDirectory: "js"
+  },
+  "rollup": {
+    name: "Rollup",
+    package: "rollup",
+    devDep: true,
+    buildDirectory: "public",
+    bundleDirectory: "./"
+  },
+  "webpack": {
+    name: "Webpack",
+    package: "webpack",
+    devDep: true,
+    buildDirectory: "dist",
+    bundleDirectory: "./"
+  }
+};
+
+const getDirectories = async (directoriesPath) =>
+  (await readdir(directoriesPath, { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory() && dirent.name.charAt(0) !== ".")
+    .map((dirent) => dirent.name);
+
+(async () => {
+  try {
+    const sampleDirs = await getDirectories(SAMPLES_PATH);
+
+    const jsapiVersion = JSON.parse(
+      await readFile(resolve(__dirname, SAMPLES_PATH, sampleDirs[0], "package.json"), "utf8")
+    ).dependencies["@arcgis/core"].replace(/\^|\~/, ""); // remove semver range
+
+    console.log(`ArcGIS JSAPI:  v${jsapiVersion}`);
+    const outputPath = resolve(__dirname, "../../esm-samples/.metrics", `${jsapiVersion}.csv`);
+    const stream = createWriteStream(outputPath);
+    stream.write("Sample,Main bundle size (MB),On-disk size (MB), On-disk files\n");
+
+    console.log("Installing dependencies and building samples");
+    await Promise.all(
+      sampleDirs.map((sample) =>
+        !!SAMPLES_INFO[sample]?.name
+          ? exec(
+              `npm i --prefix ${resolve(SAMPLES_PATH, sample)} && npm run build --prefix ${resolve(
+                SAMPLES_PATH,
+                sample
+              )}`
+            )
+          : null
+      )
+    );
+
+    for (sample of sampleDirs) {
+      const buildDir = SAMPLES_INFO[sample]?.buildDirectory;
+      const bundleDir = SAMPLES_INFO[sample]?.bundleDirectory;
+      const sampleName = SAMPLES_INFO[sample]?.name;
+      const packageName = SAMPLES_INFO[sample]?.package;
+      const isDevDep = SAMPLES_INFO[sample]?.devDep;
+
+      if (!!buildDir) {
+        const samplePath = resolve(SAMPLES_PATH, sample);
+        const buildPath = resolve(samplePath, buildDir);
+
+        const packageFile = JSON.parse(await readFile(resolve(samplePath, "package.json"), "utf8"));
+
+        const packageVersion = (
+          !!isDevDep ? packageFile.devDependencies[packageName] : packageFile.dependencies[packageName]
+        ).replace(/\^|\~/, "");
+
+        console.log(`${sampleName}: calculating size`);
+        const buildSize = (await exec(`du -sh ${buildPath} | cut -f1`)).stdout.trim().replace(/[a-z]/i, "");
+        const fileCount = (await exec(`find ${buildPath} -type f | wc -l`)).stdout.trim();
+        const mainBundleSize = Number(
+          (
+            await exec(
+              `find ${resolve(
+                buildPath,
+                bundleDir
+              )} -name '*.js' -type f -printf "%s\t%p\n" | sort -nr | head -1 | cut -f1`
+            )
+          ).stdout.trim() / 1e6 // convert bytes to megabytes
+        )
+          .toFixed(1)
+          .toString();
+
+        stream.write(`${sampleName} ${packageVersion},${mainBundleSize},${buildSize},${fileCount}\n`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  }
+})();
