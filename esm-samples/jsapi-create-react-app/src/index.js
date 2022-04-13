d1f9fdcb257acb60ea727a490cccbde82cf29a3b
Andy G <agup@esri.com>
2022-04-13 16:08:31 -0600

    Minor cleanup in React sample

--- esm-samples/jsapi-create-react-app/src/index.js
+++ esm-samples/jsapi-create-react-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
