f067e86a94287b08703fae953620d5a2f7edfb0f
Andy G <agup@esri.com>
2022-11-01 15:55:39 -0600

    Updates to new React sample vite.config.js

--- esm-samples/jsapi-react/vite.config.js
+++ esm-samples/jsapi-react/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000
+  },
+  server: {
+    open: true
+  }  
 })
