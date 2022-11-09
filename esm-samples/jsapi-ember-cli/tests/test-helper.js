7e17b88487852dd300c0d7b1eb42ccd893245460
crowjonah <crowjonah@gmail.com>
2022-11-09 09:36:31 -0500

    v3.20.0...v3.24.0

--- esm-samples/jsapi-ember-cli/tests/test-helper.js
+++ esm-samples/jsapi-ember-cli/tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'ember-cli-app/app';
 import config from 'ember-cli-app/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
