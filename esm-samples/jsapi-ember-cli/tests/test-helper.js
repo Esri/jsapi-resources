7ff2c59f13fdef3c28e4ac6de0083fbce4c6749a
crowjonah <crowjonah@gmail.com>
2022-11-09 10:35:01 -0500

    ember 4 prep

--- esm-samples/jsapi-ember-cli/tests/test-helper.js
+++ esm-samples/jsapi-ember-cli/tests/test-helper.js
@@ -1,6 +1,6 @@
 import Application from 'ember-cli-app/app';
 import config from 'ember-cli-app/config/environment';
-import * as QUnit from 'qunit';
+import QUnit from 'qunit'
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
