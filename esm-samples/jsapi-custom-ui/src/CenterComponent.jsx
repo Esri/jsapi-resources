26e5438c0b84209bc10128ab9c6aba4c9a5a44fe
Andy G <agup@esri.com>
2023-07-10 16:23:08 -0600

    Fix spelling error

--- esm-samples/jsapi-custom-ui/src/CenterComponent.jsx
+++ esm-samples/jsapi-custom-ui/src/CenterComponent.jsx
@@ -6,7 +6,7 @@ import './center-component.css';
  * React component that can be used in an ArcGIS JS SDK application.
  * This component dynamically displays the center of the map extent
  * @param {*} view an instance of a `Mapview` or `SceneView`
- * @param {string} id - The `id` of the compenents's parent HTML div element
+ * @param {string} id - The `id` of the components's parent HTML div element
  * @returns The components's HTML div
  */
 const CenterComponent = ({ view, id }) => {
