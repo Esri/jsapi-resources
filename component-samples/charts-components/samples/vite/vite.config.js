1f6cf8847be89700ecda426f5b29b69de86b9912
jchen <hchen@esri.com>
2024-11-06 14:03:13 -0800

    docs(charts-components): addressed feedback

--- component-samples/charts-components/samples/vite/vite.config.js
+++ component-samples/charts-components/samples/vite/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   server: {
     open: true,
--- component-samples/charts-components/samples/webpack/webpack.config.js
+++ component-samples/charts-components/samples/webpack/webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: ['./src/index.css', './src/index.js'],
+    index: ["./src/index.css", "./src/index.js"],
   },
   node: false,
   optimization: {
     minimizer: [new TerserPlugin({ extractComments: false })],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    chunkFilename: 'chunks/[id].js',
-    publicPath: '',
+    path: path.resolve(__dirname, "dist"),
+    chunkFilename: "chunks/[id].js",
+    publicPath: "",
     clean: true,
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
     port: 8080,
@@ -29,26 +29,26 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        enforce: 'pre',
-        use: ['source-map-loader'],
+        enforce: "pre",
+        use: ["source-map-loader"],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Charts components Webpack sample',
-      template: './public/index.html',
-      filename: './index.html',
-      chunksSortMode: 'none',
-      inlineSource: '.(css)$',
+      title: "Charts components Webpack sample",
+      template: "./public/index.html",
+      filename: "./index.html",
+      chunksSortMode: "none",
+      inlineSource: ".(css)$",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[chunkhash].css',
-      chunkFilename: '[id].css',
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[id].css",
     }),
   ],
 };
