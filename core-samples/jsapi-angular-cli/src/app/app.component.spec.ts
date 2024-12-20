4ec5f3f255bcfe65ea882f5ed027f3cd3e735b54
Andy G <agup@esri.com>
2024-12-19 17:39:10 -0700

    Angular - bump to v19

--- core-samples/jsapi-angular-cli/src/app/app.component.spec.ts
+++ core-samples/jsapi-angular-cli/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {        
+  beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
