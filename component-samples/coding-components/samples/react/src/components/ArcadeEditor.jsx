2a4b3dcd68368789187824eaeeb7ad336fe28fd7
Omar Kawach <OKawach@esri.com>
2025-01-08 14:32:10 -0800

    chore: fix event names, and prettier formatting

--- component-samples/coding-components/samples/react/src/components/ArcadeEditor.jsx
+++ component-samples/coding-components/samples/react/src/components/ArcadeEditor.jsx
@@ -35,44 +35,44 @@ export default function ArcadeEditor() {
       <div className="editor-wrapper">
         {data ? (
           <arcgis-arcade-editor
-          // Set the script on the editor
-          script="$feature"
-          // Log script change events
-          onArcgisScriptChange={async (e) => {
-            console.log("script:", e.detail);
-            // console.log("outputType on script:", await arcadeEditorElt.getOutputType());
-          }}
-          // Log editor diagnostics
-          onArcgisDiagnosticsChange={async (e) => {
-            console.log("diagnostics:", e.detail);
-          }}
-          // Tells Arcade editor to use the 'popup' profile and provides the necessary data used as
-          // definition for the profile variables. Feature Layer and Web Map instances are used by the
-          // Editor UX to help users understand the structure of data used.
-          // Note that for the $feature variable, we pass the feature layer instance as for definition
-          // the editor needs the metadata of the feature not an actual feature.
-          profile={{
-            id: "popup",
-            definitions: {
-              $feature: data.featureLayer,
-              $layer: data.featureLayer,
-              $map: data.webMap,
-              $datastore: data.featureLayer
-            }
-          }}
-          // Tells Arcade editor to the following test data. The data provided must match the expected data for the
-          // profile used.
-          // Note that for test data, the feature must an instance of a feature. This is not used for user experience
-          // but for actually executing the the Arcade expression in the editor.
-          testData={{
-            profileVariableInstances: {
-              $feature: data.featureSet.features[0],
-              $layer: data.featureLayer,
-              $map: data.webMap,
-              $datastore: data.featureLayer.url
-            }
-          }}
-        />
+            // Set the script on the editor
+            script="$feature"
+            // Log script change events
+            onarcgisScriptChange={async (e) => {
+              console.log("script:", e.detail);
+              // console.log("outputType on script:", await arcadeEditorElt.getOutputType());
+            }}
+            // Log editor diagnostics
+            onarcgisDiagnosticsChange={async (e) => {
+              console.log("diagnostics:", e.detail);
+            }}
+            // Tells Arcade editor to use the 'popup' profile and provides the necessary data used as
+            // definition for the profile variables. Feature Layer and Web Map instances are used by the
+            // Editor UX to help users understand the structure of data used.
+            // Note that for the $feature variable, we pass the feature layer instance as for definition
+            // the editor needs the metadata of the feature not an actual feature.
+            profile={{
+              id: "popup",
+              definitions: {
+                $feature: data.featureLayer,
+                $layer: data.featureLayer,
+                $map: data.webMap,
+                $datastore: data.featureLayer,
+              },
+            }}
+            // Tells Arcade editor to the following test data. The data provided must match the expected data for the
+            // profile used.
+            // Note that for test data, the feature must an instance of a feature. This is not used for user experience
+            // but for actually executing the the Arcade expression in the editor.
+            testData={{
+              profileVariableInstances: {
+                $feature: data.featureSet.features[0],
+                $layer: data.featureLayer,
+                $map: data.webMap,
+                $datastore: data.featureLayer.url,
+              },
+            }}
+          />
         ) : (
           <calcite-scrim loading />
         )}
