fd273026b3659d5436d44e1b209d7333726cd9c2
Andy <agup@esri.com>
2025-02-24 16:05:09 -0700

    chore(jsapi-node): update to use projectOperator (#556)
    * chore(jsapi-node): update to use projectOperator
    
    * Wording updates
    
    * ci: add zip files
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- core-samples/jsapi-node/src/projection.js
+++ core-samples/jsapi-node/src/projection.js
@@ -1,13 +1,15 @@
 import esriConfig from "@arcgis/core/config.js";
-import * as pe from "@arcgis/core/geometry/projection.js";
+import * as projectOperator from "@arcgis/core/geometry/operators/projectOperator.js";
 import Point from "@arcgis/core/geometry/Point.js";
 
-// Required: Set this property to insure assets resolve correctly.
+// Required: Set this property to ensure assets resolve correctly.
+// Assets include wasm, styles, images, web workers and localization files.
 esriConfig.assetsPath = "node_modules/@arcgis/core/assets"; // relative to when running in root
 
-pe.load()
+// Load the projectOperator dependencies first.
+projectOperator.load()
   .then(() => {
-    console.log(pe.project(new Point({ x: -117, y: 34 }), { wkid: 3857 }).toJSON());
+    console.log(projectOperator.execute(new Point({ x: -117, y: 34 }), { wkid: 3857 }).toJSON());
   })
   .catch((error) => {
     console.error(error);
